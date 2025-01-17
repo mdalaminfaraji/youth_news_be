2067e7924ad690b015daff9d5686f169424ee6a6
mdalaminfaraji <alaminice1617@gmail.com>
2025-01-18 01:01:21 +0600

    change plugins.js file

--- config/plugins.js
+++ config/plugins.js
@@ -3,11 +3,13 @@ module.exports = ({ env }) => ({
     config: {
       provider: "aws-s3",
       providerOptions: {
-        accessKeyId: env("AWS_ACCESS_KEY_ID"),
-        secretAccessKey: env("AWS_SECRET_ACCESS_KEY"),
-        region: env("AWS_REGION"),
-        params: {
-          Bucket: env("AWS_BUCKET"),
+        s3Options: {
+          accessKeyId: env("AWS_ACCESS_KEY_ID"),
+          secretAccessKey: env("AWS_SECRET_ACCESS_KEY"),
+          region: env("AWS_REGION"),
+          params: {
+            Bucket: env("AWS_BUCKET"),
+          },
         },
       },
     },
